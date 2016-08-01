e914890652db8a3ffe9b54272b499b299b9188d7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2016-08-01 10:38:19 +0900

    add aws_security_group.http_inbound

--- tf_conf/test.tf
+++ tf_conf/test.tf
@@ -18,6 +18,18 @@ resource "aws_security_group" "ssh_inbound" {
   }
 }
 
+resource "aws_security_group" "http_inbound" {
+  name = "http_inbound"
+  description = "Allow http inbound traffic"
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_key_pair" "default" {
   key_name = "test-key"
   public_key = "${var.ssh_public_key}"
@@ -27,7 +39,7 @@ resource "aws_instance" "test" {
   ami           = "ami-a21529cc"
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.default.key_name}"
-  security_groups = ["${aws_security_group.ssh_inbound.name}"]
+  security_groups = ["${aws_security_group.ssh_inbound.name}", "${aws_security_group.http_inbound.name}"]
   tags {
     Name = "tf test"
   }
